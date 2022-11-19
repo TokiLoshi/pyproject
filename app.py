b37d13e75d11642498c512c7970f74ed315037e4
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2022-11-19 12:49:13 -0800

    finished routes

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-# from flask import login_required
+from tempfile import mkdtemp
+from helpers import login_required
+from werkzeug.security import check_password_hash, generate_password_hash
 import os
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash 
 from cs50 import SQL
+import os
+from project import main, get_date_time
 
 # Flask login managerhttps://flask-login.readthedocs.io/en/latest/
 # login_manager = LoginManager()
@@ -20,6 +25,19 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+# Configure CS50 Library to use SQLite and adapt to tranfer to PostGres for Heroku deployment
+# https://cs50.readthedocs.io/heroku/
+# uri = os.getenv("DATABASE_URL")
+# if uri.startswith("postgres:"):
+#   uri = uri.replace("postgress://", "postgresql://")
+
+db = SQL("sqlite:///news.db")
+
+# uri = os.getenv("DATABASE_URL")
+# if uri.startswith("postgres://"):
+#     uri = uri.replace("postgres://", "postgresql://")
+# db = SQL(uri)
 
 # Handle cache (from CS50 PSET 9)
 @app.after_request
@@ -51,13 +69,20 @@ def login():
   """Admin wants to login"""
   #Forgets user_id
   session.clear()
+
   if request.method == "POST":
     username = request.form.get("email")
     password = request.form.get("password")
+    print("Username: ", username)
+    print("Password: ", password)
 
-    if username == "TokiLoshiCodes" and password == "rumbat":
+    if username == "silvabee@gmail.com" and password == "wombat":
       print("Valid username and password")
-      redirect("/news")
+      session["user_id"] = 1
+      return redirect("/news")
+    elif username == "visitor@notarealemail.com" and password == "curiouser":
+      session["user_id"] = 2
+      return redirect("/news")
     else:
       flash("Something went wrong")
       return  render_template("/login.html")
@@ -66,14 +91,27 @@ def login():
 
 # News page
 @app.route("/news", methods=["GET", "POST"])
-# @login_required
+@login_required
 def news():
   """Show Admin the news"""
   if request.method == "POST":
     flash("News Time")
-    print("Time to get the news")
-    return render_template("news.html")
+    return redirect("/news")
   else:
-    flash("Get request, and we can get the news")
-    print("Time to get the news")
-    return render_template("news.html")
+    flash("News Time")
+    tech_news, sf_news, africa_news, sa_news = main()
+    current_sf, current_eastcoast, current_time_europe, current_time_africa = get_date_time()
+    print(current_sf)
+    print(current_eastcoast)
+    print(current_time_europe)
+    print(current_time_africa)
+
+    return render_template("news.html", africatime=current_time_africa, zurichtime=current_time_europe, nytime=current_eastcoast, sftime=current_sf, tech_news=tech_news, sf_news=sf_news, africa_news=africa_news, sa_news=sa_news)
+
+# Log out
+@app.route("/logout", methods=["GET", "POST"])
+@login_required
+def logout():
+  """Log out"""
+  session.clear()
+  return render_template("/login.html")
--- test_project.py
+++ test_project.py
@@ -1,3 +1,39 @@
 import pytest
+from project import get_tech_news, get_sf_news, get_sa_news, get_africa_news, get_date_time
+import requests 
+from bs4 import BeautifulSoup
+from datetime import datetime
+from pytz import timezone
+import pytz 
 
 # Need to test each function throughly
+def test_tech_news():
+  tech_url = "https://techcrunch.com/"
+  response = requests.get(tech_url)
+  assert type(response) == requests.models.Response
+  # Tutorial on checking response status codes: https://medium.com/@novyludek/custom-status-code-matcher-for-api-testing-in-python-with-pyhamcrest-43183d65475d
+  assert response.status_code == 200
+
+
+def test_sf_news():
+  sf_url = "https://sfist.com/news/"
+  response = requests.get(sf_url)
+  assert type(response) == requests.models.Response
+  assert response.status_code == 200
+
+def test_sa_news():
+  sa_url = "https://www.dailymaverick.co.za/section/maverick-news/"
+  response = requests.get(sa_url)
+  assert type(response) == requests.models.Response
+  assert response.status_code == 200
+
+def test_africa_news():
+  africa_url = "https://allafrica.com/mozambique/?page=1"
+  response = requests.get(africa_url)
+  assert type(response) == requests.models.Response
+  assert response.status_code != 500
+  assert response.status_code == 200
+
+
+def test_date_time():
+  ...
