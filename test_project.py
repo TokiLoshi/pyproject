d88381965c0cf87e6284fa4cd6a4db405ecef5d0
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2022-11-22 17:45:28 -0800

    Cleaned up testing

--- test_project.py
+++ test_project.py
@@ -8,30 +8,34 @@ import pytz
 from unittest.mock import Mock 
 import datetime
 
-# Tutorial on using Mock https://realpython.com/python-mock-library/
+# Tutorial on using Mock from real python https://realpython.com/python-mock-library/
+# Tutorial on intro to Mock https://www.youtube.com/watch?v=xT4SV7AH3G8&list=PLe4mIUXfbIqaXv8kRvUqgrMREq-p5GgdV&index=1
 
 mock = Mock()
-  # print("HEADLINES IN MAIN: ", headlines)
 
-
-# Need to test each function throughly
-def test_tech_news():
+# Testing get_tech_news
+def test_get_tech_news():
+  # Check for a successful request status code
   mock.requests.return_value = 200
   assert mock.requests() == 200
   headline_list = []
   tech_news = []
 
+  # Open the mock folder with saved snipped of code
   with open("technews.txt") as file:
     for line in file:
       techsoup = BeautifulSoup(line, "lxml")
       headline_list.append(techsoup)
   file.close()
   
+  # Clean the headlines
+  tech_news_stories = []
   for story in headline_list:
     for headline in story.stripped_strings:
       techie_headlines = repr((headline))
-      tech_news.append(techie_headlines)
+      tech_news_stories.append(techie_headlines)
 
+  # Extract the links
   links = []
   for story in headline_list:
     headlines = story.find_all("a")
@@ -39,19 +43,23 @@ def test_tech_news():
       actual_headlines = headline["href"]
       links.append(actual_headlines)
 
-  mock.first_headline.return_value = tech_news
+  # Combine the lists as was done in project 
+  combined_tech_news = [item for sublist in zip(tech_news_stories, links) for item in sublist]
+
+  # Create mock object with the value of combined lists
+  mock.first_headline.return_value = combined_tech_news
   
   # Check headlines are correct
-  assert mock.first_headline() == tech_news
+  assert mock.first_headline() == combined_tech_news
   
   # Check that the correct number of headlines are returned
-  assert len(tech_news) == 21
+  assert len(tech_news_stories) == 21
 
   # Check headlines are what they are supposed to be
 
-  tech_news[-1] == "Ransomware is a global problem that needs a global solution"
-  tech_news[11] == "Elizabeth Holmes sentenced to 11 years in prison for Theranos fraud"
-  tech_news[-3] ==  'India’s securities depository CDSL says malware compromised its network'
+  tech_news_stories[-1] == "Ransomware is a global problem that needs a global solution"
+  tech_news_stories[11] == "Elizabeth Holmes sentenced to 11 years in prison for Theranos fraud"
+  tech_news_stories[-3] ==  'India’s securities depository CDSL says malware compromised its network'
   
   # Check links is only links
   mock.onlylinks.return_value = links
@@ -63,9 +71,9 @@ def test_tech_news():
   assert links[0].startswith('https')
 
   # Check the right key words match in the links
-  list_key_words_one = tech_news[-1].split(" ")  
-  list_key_words_eleven = tech_news[11].split(" ")
-  list_key_words_third_last = tech_news[-3].split(" ")
+  list_key_words_one = tech_news_stories[-1].split(" ")  
+  list_key_words_eleven = tech_news_stories[11].split(" ")
+  list_key_words_third_last = tech_news_stories[-3].split(" ")
   key_word_one = ((list_key_words_one[0]).replace("\'", "")).lower()
   key_word_eleven = ((list_key_words_eleven[0]).replace("\'", "")).lower()
 
@@ -76,9 +84,11 @@ def test_tech_news():
   # check program is getting the correct headlines and links
   assert len(links) == 21
   # check each story has a link
-  assert len(tech_news) == len(links)
+  assert len(tech_news_stories) == len(links)
 
-def test_sf_news():
+# Test get_sf_news
+def test_get_sf_news():
+  # Check for a successful request status code
   mock.requests.return_value = 200
   assert mock.requests() == 200
   sf_results = []
@@ -98,7 +108,9 @@ def test_sf_news():
   # Check headlines are correct
   assert mock.first_headline() == sf_headlines
 
-def test_sa_news():
+# Test get_sa_news
+def test_get_sa_news():
+  # Check for a successful request status code
   mock.requests.return_value = 200
   assert mock.requests() == 200
 
@@ -128,38 +140,43 @@ def test_sa_news():
   mock.test_articles.return_value = sa_articles
   assert mock.test_articles() == sa_articles
 
-
-def test_africa_news():
+# Test get_africa_news
+def test_get_africa_news():
+  # Check for a successful request status code
   mock.requests.return_value = 200
   assert mock.requests() == 200
 
   africa_results = []
+  # Open code snippet to mock 
   with open("africanews.txt") as file:
     for line in file:
       africa_results.append(BeautifulSoup(line, "lxml").find_all("p"))
 
+  # Check the results match 
   mock.africa_results.return_value = africa_results
   assert mock.africa_results() == africa_results
   assert len(africa_results) > 0
 
+  # Extract headlines 
   africa_headlines = []
   for line in africa_results:
     for element in line:
       africa_headlines.append(element.text)
 
+  # Check length of headlines
   assert len(africa_headlines) > 0
   mock.africa_headlines.return_value = africa_headlines
   assert mock.africa_headlines() == africa_headlines
   
-
-def test_date_time():
-  # Tutorial on using datetime and the mock library
-  # Testing during daylight savings
+# Test get_date_time
+def test_get_date_time():
+  # Tutorial on using datetime and the mock library https://realpython.com/python-mock-library/
   mock_sf_time = datetime.datetime(year = 2022, month = 11, day=19, hour= 7, minute = 10)
   mock_ny_time = datetime.datetime(year = 2022, month = 11, day = 19, hour = 10, minute = 10)
   mock_zh_time = datetime.datetime(year = 2022, month = 11, day = 19, hour = 16, minute = 10)
   mock_sa_time = datetime.datetime(year = 2022, month = 11, day = 19, hour = 15, minute = 10)
-  
+
+  # Check mock objects return the mock values set 
   mock.sf_time.return_value = mock_sf_time
   assert mock.sf_time() == mock_sf_time
   mock.ny_time.return_value = mock_ny_time
